b0d53395ab5f3d90adad1bb503bbf128baf1a4eb
anderson <anderson.d.cardoso92@gmail.com>
2022-05-19 20:10:14 -0300

    add 3d model to hero section and projects component

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,8 +1,13 @@
 import styles from '../styles/Hero.module.css';
+import ThreeCanvas from './ThreeCanvas';
 
 const Hero = () => {
   return (
     <div className={styles.hero}>
+    <div className={styles.heroText}>
+        {/* <h2>hello, I'm Anderson</h2> */}
+    </div>
+    <ThreeCanvas />
     </div>
   )
 }
---
+++ components/Projects.jsx
@@ -0,0 +1,11 @@
+import styles from '../styles/Projects.module.css';
+
+const Projects = () => {
+  return (
+    <div className={styles.projects}>
+        <h2>Projects</h2>
+    </div>
+  )
+}
+
+export default Projects
