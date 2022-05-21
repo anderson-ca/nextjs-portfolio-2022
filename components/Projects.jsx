6de7a8541c6822bf4d21ef7dd5ce834457bb51d2
anderson <anderson.d.cardoso92@gmail.com>
2022-05-21 16:37:31 -0300

    adding alider functionality

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -22,100 +22,126 @@ const Projects = () => {
   return (
     <div className={styles.projectsWrapper}>
       <h2 className={styles.h2}>My Projects</h2>
-      <ul className={styles.projectList}>
-        <li className={styles.projectItem}>
-          <div className={styles.card}>
-            <div
-              onMouseEnter={() => setHover(true)}
-              onMouseLeave={() => setHover(false)}
-              className={styles.imageWrapper}
-            >
-              <video
-                ref={videoPlayer}
-                id={styles.myVideo}
-                width="100%"
-                height="auto"
-                src="/carouselvid1.mp4"
-                autoPlay
-                loop
-                muted
+      <div className={styles.main}>
+        <div>
+          <Link href="/" passHref>
+            <a className={styles.techLink}>
+              <Image
+                src="/arrow-left.svg"
+                alt="source code icon"
+                width={"40%"}
+                height={"40%"}
               />
+            </a>
+          </Link>
+        </div>
+        <ul className={styles.projectList}>
+          <li className={styles.projectItem}>
+            <div className={styles.card}>
+              <div
+                onMouseEnter={() => setHover(true)}
+                onMouseLeave={() => setHover(false)}
+                className={styles.imageWrapper}
+              >
+                <video
+                  ref={videoPlayer}
+                  id={styles.myVideo}
+                  width="100%"
+                  height="auto"
+                  src="/carouselvid1.mp4"
+                  autoPlay
+                  loop
+                  muted
+                />
+              </div>
+              <div className={styles.cardTextWrapper}>
+                <h3 className={styles.h3}>Title</h3>
+                <p className={styles.p}>
+                  This is a simple project built using html, css, javascript,
+                  and Three.js
+                </p>
+                <ul className={styles.techList}>
+                  <li>
+                    <Link href="/" passHref>
+                      <a className={styles.techLink}>
+                        <Image
+                          src="/html.svg"
+                          alt="source code icon"
+                          width={"40%"}
+                          height={"40%"}
+                        />
+                      </a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/" passHref>
+                      <a className={styles.techLink}>
+                        <Image
+                          src="/css.svg"
+                          alt="source code icon"
+                          width={"40%"}
+                          height={"40%"}
+                        />
+                      </a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/" passHref>
+                      <a className={styles.techLink}>
+                        <Image
+                          src="/js.svg"
+                          alt="source code icon"
+                          width={"40%"}
+                          height={"40%"}
+                        />
+                      </a>
+                    </Link>
+                  </li>
+                </ul>
+                {/* ///////////// */}
+                <ul className={styles.projectIcons}>
+                  <li>
+                    <Link href="/" passHref>
+                      <a className={styles.srcLink}>
+                        <Image
+                          src="/code.svg"
+                          alt="source code icon"
+                          width={"100%"}
+                          height={"30%"}
+                        />
+                      </a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/" passHref>
+                      <a className={styles.srcLink}>
+                        <Image
+                          src="/website.svg"
+                          alt="website icon"
+                          width={"100%"}
+                          height={"40%"}
+                        />
+                      </a>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
             </div>
-            <div className={styles.cardTextWrapper}>
-              <h3 className={styles.h3}>Title</h3>
-              <p className={styles.p}>
-                This is a simple project built using html, css, javascript, and
-                Three.js
-              </p>
-              <ul className={styles.techList}>
-                <li>
-                  <Link href="/" passHref>
-                    <a className={styles.techLink}>
-                      <Image
-                        src="/html.svg"
-                        alt="source code icon"
-                        width={"40%"}
-                        height={"40%"}
-                      />
-                    </a>
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/" passHref>
-                    <a className={styles.techLink}>
-                      <Image
-                        src="/css.svg"
-                        alt="source code icon"
-                        width={"40%"}
-                        height={"40%"}
-                      />
-                    </a>
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/" passHref>
-                    <a className={styles.techLink}>
-                      <Image
-                        src="/js.svg"
-                        alt="source code icon"
-                        width={"40%"}
-                        height={"40%"}
-                      />
-                    </a>
-                  </Link>
-                </li>
-              </ul>
-              {/* ///////////// */}
-              <ul className={styles.projectIcons}>
-                <li>
-                  <Link href="/" passHref>
-                    <a className={styles.srcLink}>
-                      <Image
-                        src="/code.svg"
-                        alt="source code icon"
-                        width={"100%"}
-                        height={"30%"}
-                      />
-                    </a>
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/" passHref>
-                    <a className={styles.srcLink}>
-                      <Image
-                        src="/website.svg"
-                        alt="website icon"
-                        width={"100%"}
-                        height={"40%"}
-                      />
-                    </a>
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </li>
-      </ul>
+          </li>
+        </ul>
+        <div>
+          <Link href="/" passHref>
+            <a className={styles.techLink}>
+              <Image
+                src="/arrow-right.svg"
+                alt="source code icon"
+                width={"40%"}
+                height={"40%"}
+              />
+            </a>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
