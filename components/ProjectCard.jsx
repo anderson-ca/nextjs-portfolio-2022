d43ef81d830cc1717a3675d7f1d116c6984c9274
anderson <anderson.d.cardoso92@gmail.com>
2022-05-22 11:46:02 -0300

    working on project card component

---
+++ components/ProjectCard.jsx
@@ -0,0 +1,116 @@
+import styles from "../styles/ProjectCard.module.css";
+import { useState, useEffect, useRef } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+function ProjectCard() {
+  const videoPlayer = useRef(null);
+  const [hover, setHover] = useState(false);
+
+  useEffect(() => {
+    let video = videoPlayer.current;
+
+    if (hover) {
+      console.log("hover baby", hover);
+      video.play();
+    } else {
+      console.log("not hovering :(", hover);
+      video.pause();
+    }
+  }, [hover]);
+
+  return (
+    <div className={`${styles.card}`}>
+      <div
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+        className={styles.imageWrapper}
+      >
+        <video
+          ref={videoPlayer}
+          id={styles.myVideo}
+          width="100%"
+          height="auto"
+          src="/carouselvid1.mp4"
+          autoPlay
+          loop
+          muted
+        />
+      </div>
+      <div className={styles.cardTextWrapper}>
+        <h3 className={styles.h3}>Title</h3>
+        <p className={styles.p}>
+          This is a simple project built using html, css, javascript, and
+          Three.js
+        </p>
+        <ul className={styles.techList}>
+          <li>
+            <Link href="/" passHref>
+              <a className={styles.techLink}>
+                <Image
+                  src="/html.svg"
+                  alt="source code icon"
+                  width={"40%"}
+                  height={"40%"}
+                />
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/" passHref>
+              <a className={styles.techLink}>
+                <Image
+                  src="/css.svg"
+                  alt="source code icon"
+                  width={"40%"}
+                  height={"40%"}
+                />
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/" passHref>
+              <a className={styles.techLink}>
+                <Image
+                  src="/js.svg"
+                  alt="source code icon"
+                  width={"40%"}
+                  height={"40%"}
+                />
+              </a>
+            </Link>
+          </li>
+        </ul>
+
+        <ul className={styles.projectIcons}>
+          <li>
+            <Link href="/" passHref>
+              <a className={styles.srcLink}>
+                <Image
+                  src="/code.svg"
+                  alt="source code icon"
+                  width={"100%"}
+                  height={"30%"}
+                />
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/" passHref>
+              <a className={styles.srcLink}>
+                <Image
+                  src="/website.svg"
+                  alt="website icon"
+                  width={"100%"}
+                  height={"40%"}
+                />
+              </a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default ProjectCard;
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,142 +1,61 @@
 import styles from "../styles/Projects.module.css";
 import { useState, useEffect, useRef } from "react";
-import Image from "next/Image";
-import Link from "next/Link";
+import ProjectCard from "./ProjectCard";
+import Image from "next/image";
+import Link from "next/link";
 
 const Projects = () => {
   const videoPlayer = useRef(null);
   const [hover, setHover] = useState(false);
 
-  useEffect(() => {
-    let video = videoPlayer.current;
-
-    if (hover) {
-      console.log("hover baby", hover);
-      video.play();
-    } else {
-      console.log("not hovering :(", hover);
-      video.pause();
+  const range = (start, end, step = 1) => {
+    let output = [];
+    if (typeof end === "undefined") {
+      end = start;
+      start = 0;
     }
-  }, [hover]);
+    for (let i = start; i < end; i += step) {
+      output.push(i);
+    }
+    return output;
+  };
 
   return (
     <div className={styles.projectsWrapper}>
       <h2 className={styles.h2}>My Projects</h2>
       <div className={styles.main}>
-        <div>
+        <div className={styles.arrowWrapper}>
           <Link href="/" passHref>
-            <a className={styles.techLink}>
+            <a className={styles.leftArrow}>
               <Image
                 src="/arrow-left.svg"
                 alt="source code icon"
-                width={"40%"}
-                height={"40%"}
+                width={"100%"}
+                height={"100%"}
               />
             </a>
           </Link>
         </div>
         <ul className={styles.projectList}>
-          <li className={styles.projectItem}>
-            <div className={styles.card}>
-              <div
-                onMouseEnter={() => setHover(true)}
-                onMouseLeave={() => setHover(false)}
-                className={styles.imageWrapper}
+          {range(0, 1).map((num) => {
+            return (
+              <li
+                key={num}
+                className={`${styles.projectItem} ${styles.active}`}
               >
-                <video
-                  ref={videoPlayer}
-                  id={styles.myVideo}
-                  width="100%"
-                  height="auto"
-                  src="/carouselvid1.mp4"
-                  autoPlay
-                  loop
-                  muted
-                />
-              </div>
-              <div className={styles.cardTextWrapper}>
-                <h3 className={styles.h3}>Title</h3>
-                <p className={styles.p}>
-                  This is a simple project built using html, css, javascript,
-                  and Three.js
-                </p>
-                <ul className={styles.techList}>
-                  <li>
-                    <Link href="/" passHref>
-                      <a className={styles.techLink}>
-                        <Image
-                          src="/html.svg"
-                          alt="source code icon"
-                          width={"40%"}
-                          height={"40%"}
-                        />
-                      </a>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="/" passHref>
-                      <a className={styles.techLink}>
-                        <Image
-                          src="/css.svg"
-                          alt="source code icon"
-                          width={"40%"}
-                          height={"40%"}
-                        />
-                      </a>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="/" passHref>
-                      <a className={styles.techLink}>
-                        <Image
-                          src="/js.svg"
-                          alt="source code icon"
-                          width={"40%"}
-                          height={"40%"}
-                        />
-                      </a>
-                    </Link>
-                  </li>
-                </ul>
-                {/* ///////////// */}
-                <ul className={styles.projectIcons}>
-                  <li>
-                    <Link href="/" passHref>
-                      <a className={styles.srcLink}>
-                        <Image
-                          src="/code.svg"
-                          alt="source code icon"
-                          width={"100%"}
-                          height={"30%"}
-                        />
-                      </a>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="/" passHref>
-                      <a className={styles.srcLink}>
-                        <Image
-                          src="/website.svg"
-                          alt="website icon"
-                          width={"100%"}
-                          height={"40%"}
-                        />
-                      </a>
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </li>
+                <ProjectCard />
+              </li>
+            );
+          })}
         </ul>
-        <div>
+        <div className={styles.arrowWrapper}>
           <Link href="/" passHref>
-            <a className={styles.techLink}>
+            <a className={styles.rightArrow}>
               <Image
                 src="/arrow-right.svg"
                 alt="source code icon"
-                width={"40%"}
-                height={"40%"}
+                width={"100%"}
+                height={"100%"}
               />
             </a>
           </Link>
