d82738461579dbf36dbd9ca675b133f270bf69b7
anderson <anderson.d.cardoso92@gmail.com>
2022-06-23 09:53:48 -0500

    made about me section responsive

--- components/About.jsx
+++ components/About.jsx
@@ -1,15 +1,16 @@
+import image from "next/image";
 import styles from "../styles/About.module.css";
 import { useParallax } from "react-scroll-parallax";
 import AboutDivider from "./AboutDivider";
 
 const About = () => {
-  const { ref } = useParallax({ speed: 10 });
+  const { ref } = useParallax({ speed: 15 });
 
   return (
     <div className={styles.container}>
       <div ref={ref} className={styles.content}>
-        <h2 classNmae={`${styles.title}`}>About Me</h2>
-        <p classNmae={`${styles.paragraph}`}>
+        <h2 className={`${styles.title}`}>About Me</h2>
+        <p className={`${styles.paragraph}`}>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde,
           deserunt, sed ullam iure ratione maxime delectus dignissimos ipsum
           earum voluptate in sequi dolorum aliquam ipsam iusto sapiente? Hic,
