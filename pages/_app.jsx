85bd18887ced797a65f3eebca87399ff87747520
anderson <anderson.d.cardoso92@gmail.com>
2022-06-28 20:07:09 -0500

    add loading spinner and fix navigation text color issue

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,23 @@
 import "../styles/globals.css";
+import { useState, useEffect } from "react";
 import { AppWrapper } from "../context/state";
 import { ParallaxProvider } from "react-scroll-parallax";
+import { PacmanLoader } from "react-spinners";
 
 function MyApp({ Component, pageProps }) {
-  return (
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, Math.random() * 4000);
+  }, []);
+
+  return loading ? (
+    <div className={"spinner"}>
+      <PacmanLoader color={"#FF4C7A"} loading={true} size={"4vw"} />
+    </div>
+  ) : (
     <AppWrapper>
       <ParallaxProvider>
         <Component {...pageProps} />
--- pages/index.jsx
+++ pages/index.jsx
@@ -6,31 +6,16 @@ import Contact from "../components/Contact";
 import About from "../components/About";
 import Footer from "../components/Footer";
 import { useState, useEffect } from "react";
-import { PacmanLoader } from "react-spinners";
 
 export default function Home() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setLoading(false);
-  }, []);
-
   return (
     <div className={styles.container}>
-      {loading ? (
-        <div className={styles.spinner}>
-          <PacmanLoader color={"#FF4C7A"} loading={true} size={"4vw"} />
-        </div>
-      ) : (
-        <>
-          <Navbar />
-          <Hero />
-          <Projects />
-          <Contact />
-          <About />
-          <Footer />
-        </>
-      )}
+      <Navbar />
+      <Hero />
+      <Projects />
+      <Contact />
+      <About />
+      <Footer />
     </div>
   );
 }
